03bbddcc12ee0671334fda4b0e4f45db34f4385b
Aaron Nguyen <aaronnguyen.me@outlook.com>
2021-04-08 15:26:51 -0400

    Reoraganize directories

---
+++ Play.Common/src/Play.Common/IRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Play.Common
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetAsync(Guid id);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task PostAsync(T entity);
+        Task PutAsync(T entity);
+        Task DeleteAsync(Guid id);
+    }
+
+}
